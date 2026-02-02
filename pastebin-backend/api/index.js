22a31cd4fa741ce3cb1bb502cfdd199f3ea31028
swagatamath <110287086+swagatamath@users.noreply.github.com>
2026-02-02 10:08:51 +0530

    Add CORS support to API with OPTIONS method

--- pastebin-backend/api/index.js
+++ pastebin-backend/api/index.js
@@ -7,7 +7,12 @@ const { initTable } = require("../src/services/paste.service");
 
 const app = express();
 
-
+app.options('*', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.status(200).end();
+});   
 
 /* ------------------------------------- */
 
